1ac2d6b2ed2bd4ecf46187e6f5806c71db9573ca
OmarEbada <omarebada327@gmail.com>
2026-07-05 19:49:57 +0300

    authService file completed now i will work in controllers file

---
+++ Backend/services/authService.js
@@ -0,0 +1,26 @@
+const brcypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const { findUserByEmail } = require("./userService");
+
+const loginUser = async (email, password) => {
+    const user = await findUserByEmail(email);
+    if (!user) {
+        throw new Error("Invaild email or password");
+    }
+
+    const isMatch = await brcypt.compare(password, user.password);
+
+    if (!isMatch) {
+        throw new Error("Invaild email or password")
+    }
+
+    const token = jwt.sign(
+        { id: user._id, role: user.role },
+        process.env.JWT_SECRET,
+        { expiresIn: process.env.JWT_EXPIRES_IN }
+    );
+
+    return { user, token };
+};
+
+module.exports = { loginUser };
