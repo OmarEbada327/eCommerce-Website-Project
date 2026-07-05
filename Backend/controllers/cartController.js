a6090c764caaf61a9bf7ed91bda87e520c8e71f8
OmarEbada <omarebada327@gmail.com>
2026-07-06 00:42:13 +0300

    cartController file Completed

--- Backend/controllers/cartController.js
+++ Backend/controllers/cartController.js
@@ -11,5 +11,46 @@ const getCart = async (req, res) => {
 };
 
 const addToCart = async (req, res) => {
-    
-}
+    try {
+        const { productId, quantity } = req.body;
+        const cart = await cartService.addItemToCart(req.user.id, productId, quantity);
+        res.status(200).json(cart);
+    }
+    catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+};
+
+const updateCartItem = async (req, res) => {
+    try {
+        const { productId, quantity } = req.body;
+        const cart = await cartService.updateCartItem(req.user.id, productId, quantity);
+        res.status(200).json(cart);
+    }
+    catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+};
+
+
+const removeFromCart = async (req, res) => {
+    try {
+        const cart = await cartService.removeItemFromCart(req.user.id, req.params.productId);
+        res.status(200).json(cart);
+    }
+    catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+};
+
+const clearCart = async (req, res) => {
+    try {
+        const cart = await cartService.clearCart(req.user.id);
+        res.status(200).json(cart);
+    }
+    catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+};
+
+module.exports = { getCart, addToCart, updateCartItem, removeFromCart, clearCart };
