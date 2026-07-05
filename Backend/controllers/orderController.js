81367ff631ef0388ece249c1b5a9793a260120f6
OmarEbada <omarebada327@gmail.com>
2026-07-06 02:41:14 +0300

    orderController file completed

---
+++ Backend/controllers/orderController.js
@@ -0,0 +1,53 @@
+const orderService = require("../services/orderService");
+
+const checkout = async (req, res) => {
+    try {
+        const { paymentMethod, shippingAddress } = req.body;
+        const order = await orderService.createOrderFromCart(req.user.id, paymentMethod, shippingAddress);
+        res.status(201).json(order);
+    }
+    catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+};
+
+const getMyOrders = async (req, res) => {
+    try {
+        const orders = await orderService.getOrdersByUser(req.user.id);
+        res.status(200).json(orders);
+    }
+    catch (err) {
+        res.status(500).json({ message: err.message});
+    }
+};
+
+const getOrder = async (req, res) => {
+    try {
+        const order = await orderService.getOrderById(req.params.id);
+        res.status(200).json(order);
+    }
+    catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+const updateOrderStatus = async (req, res) => {
+    try {
+        const order = await orderService.updateOrderStatus(req.params.id, req.body.status);
+        res.json(200).json(order);
+    }catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+};
+
+const cancelOrder = async (req, res) => {
+    try {
+        const order = await orderService.cancelOrder(req.params.id);
+        res.status(200).json(order);
+    }
+    catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+};
+
+module.exports = { checkout, getMyOrders, getOrder, updateOrderStatus, cancelOrder };
