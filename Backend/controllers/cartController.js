94e61148ba738ba7715ab24f897e4f5e517a94d5
OmarEbada <omarebada327@gmail.com>
2026-07-05 21:03:33 +0300

    cartController P1

--- Backend/controllers/cartController.js
+++ Backend/controllers/cartController.js
@@ -1,2 +1,15 @@
 const cartService = require("./../services/cartService");
 
+const getCart = async (req, res) => {
+    try {
+        const cart = await cartService.getCartByUser(req.user.id);
+        res.status(200).json(cart);
+    }
+    catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+};
+
+const addToCart = async (req, res) => {
+    
+}
