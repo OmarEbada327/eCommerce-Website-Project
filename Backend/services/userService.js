a9c762c0546583a6fa3ffaf7fe8604ced0fc29ce
OmarEbada <omarebada327@gmail.com>
2026-07-05 19:20:00 +0300

    User service completed now authService file

--- Backend/services/userService.js
+++ Backend/services/userService.js
@@ -9,3 +9,49 @@ const registerUser = async (data) => {
     });
     return user;
 };
+
+const findUserByEmail = async (email) => {
+    return await User.findOne({ email });
+};
+
+const getUserById = async (id) => {
+    const user = await User.findById(id).select("-password");
+    if (!user) {
+        throw new Error("User not found")
+    };
+    return user;
+};
+
+const updateUserProfile = async (id, data) => {
+    const { role, ...safeData} = data;
+
+    const user = await User.findByIdAndUpdate(id, safeData, {new: true, runValidators: true}).select("-password");
+
+    if (!user) {
+        throw new Error("User not found");
+    };
+
+    return user;
+};
+
+const updateUserRole = async (id, role) => {
+    const user = await User.findByIdAndUpdate(id, { role } , {new: true, runValidators: true}).select("-password");
+
+    if (!user) {
+        throw new Error("User not found");
+    };
+
+    return user;
+};
+
+const deleteUser = async (id) => {
+    const user = await User.findByIdAndDelete(id);
+
+    if (!user) {
+        throw new Error("User not found");
+    };
+
+    return user;
+};
+
+module.exports = { registerUser, findUserByEmail, getUserById, updateUserProfile, updateUserRole, deleteUser};
