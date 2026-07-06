70f8e8c537a54ee4ed1aa974190340a4da137a4b
OmarEbada <omarebada327@gmail.com>
2026-07-07 00:51:39 +0300

    authController file Completed

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -1,3 +1,19 @@
 const { registerUser } = require("../services/userService");
 const { loginUser } = require("../services/authService");
+const asyncHandler = require("../middleware/asyncHandler");
 
+const register = asyncHandler(async (req, res) => {
+    const user = await registerUser(req.body);
+    res.status(201).json({ id: user._id, name: user.name, email: user.email });
+});
+
+const login = asyncHandler(async (req, res) => {
+    const { email, password } = req.body;
+    const { token, user } = await loginUser(email, password);
+    res.status(200).json({
+        token,
+        user: { id: user._id, name: user.name, email: user.email, role: user.role },
+    });
+})
+
+module.exports = { register, login };
