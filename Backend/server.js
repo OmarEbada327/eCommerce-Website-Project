c9730a5c64c2882ec98d57906a8b084799ec2d22
OmarEbada <omarebada327@gmail.com>
2026-07-07 21:43:52 +0300

    Server file Completed

--- Backend/server.js
+++ Backend/server.js
@@ -1,10 +1,25 @@
 const express = require("express");
-const dotenv = require("dotenv");
 const connectDB = require("./db/connect");
 const { PORT } = require("./config/config");
+const errorHandler = require("./middleware/errorHandler");
+
+const productRoutes = require("./routes/productRoute");
+const cartRoutes = require("./routes/cartRoute");
+const orderRoutes = require("./routes/orderRoute");
+const userRoutes = require("./routes/userRoute");
+const authRoutes = require("./routes/authRoute");
 
 const app = express();
 
+app.use(express.json());
+
+app.use("/products", productRoutes);
+app.use("/cart", cartRoutes);
+app.use("/orders", orderRoutes);
+app.use("/users", userRoutes);
+app.use("/auth", authRoutes);
+
+app.use(errorHandler);
 
 connectDB().then(() => {
     app.listen(PORT, () => {
